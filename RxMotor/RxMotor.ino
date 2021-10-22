4d751317fff3248369e4c9896c033703d9df8475
jlarsch <jlarsch@rockefeller.edu>
2021-10-22 22:26:38 +0200

    Changed motor rotation counting to using interrupts

--- RxMotor/RxMotor.ino
+++ RxMotor/RxMotor.ino
@@ -18,13 +18,12 @@ unsigned long CurrentTime  = 0;     // this variable will be overwritten by micr
 unsigned long startTime     = 0;     // A record of the time before this read
 unsigned long ElapsedTime  = 0;     // Elapsed time in uS
 
-int numRot1 = 12; // number of motor rotations flying down.
+int numRot1 = 10; // number of motor rotations flying down.
 unsigned long restDelay = 6000; // time before flyback (ms)
 unsigned long startDelay=15000;
-int extraFlybackRevolution = 10; //compensation for slip.
+int extraFlybackRevolution = 1; //compensation for slip.
 
-float hall_thresh = 100.0;
-unsigned long hall_count = 1;
+volatile int hall_count = 0;
 
 const byte thisSlaveAddress[5] = {'R','x','A','A','A'};
 
@@ -33,7 +32,6 @@ RF24 radio(CE_PIN, CSN_PIN);
 int dataReceived; // this must match dataToSend in the TX
 int ackData[2] = {0, 0}; // the two values to be sent to the master
 bool newData = false;
-bool on_state = false;
 
 int state = 0;
 int trigger = 0;
@@ -63,19 +61,32 @@ void setup() {
     radio.writeAckPayload(1, &ackData, sizeof(ackData)); // pre-load data
 
     numRot=numRot1;
+
+    attachInterrupt(digitalPinToInterrupt(hallIn), hallISR, RISING);  // attach interrupt handler
 }
 
-void updateHall(){
-  bool hall = digitalRead(hallIn);
 
-  if (hall==0){
-      if (on_state==false){
-        on_state = true;
-        hall_count++;
-      }
-    } else{
-      on_state = false;
-    }
+void hallISR(){
+  hall_count++;
+  Serial.print(state);
+  Serial.print(", ");
+  Serial.print(numRot);
+  Serial.print(", ");
+  Serial.println(hall_count);
+}
+
+int get_hall_count(){
+  noInterrupts();
+  int tmp=hall_count;
+  interrupts();
+  return tmp;
+}
+
+void reset_hall_count(){
+  noInterrupts();
+  hall_count=0;
+  interrupts();
+  // Serial.println("hall_count reset");
 }
 
 //==========
@@ -85,21 +96,21 @@ void loop() {
   int pwmOutput = map(potValue, 0, 1023, 0 , 255); // Map the potentiometer value from 0 to 255
   
 
-    updateHall();
-      
-    if (hall_count>numRot && state!= 2){
-      //hall_count=0;
+    int hall_count_now=get_hall_count();
+    if (hall_count_now>=numRot && state!= 2){
       state+=1;
       if (state>3) {
         state=0;
-        delay(5000);
+        //delay(5000);
+        reset_hall_count();
       }
     }
 
     if (state==0){
         analogWrite(enA, 0); // Send stop signal to L298N Enable pin
         numRot=numRot1;
-        hall_count=0;
+        reset_hall_count();
+        
     }
     else if (state==1){
       digitalWrite(in1, LOW);
@@ -107,31 +118,32 @@ void loop() {
       analogWrite(enA, pwmOutput); // Send PWM signal to L298N Enable pin
     }
 
-        else if (state==2){
+    else if (state==2){
       analogWrite(enA, 0); // Send stop signal to L298N Enable pin
-
       startTime = millis();
-
-    ElapsedTime = 0;
-    while (ElapsedTime < restDelay)
-    {
-      CurrentTime = millis();
-      updateHall(); // keep counting revolutions in state 2. Ghost continues to fly even when motor is off.
-          
-    
-    Serial.print(state);
-    Serial.print(", waiting for delay,  ");
-    Serial.print(ElapsedTime);
-    Serial.print(",  ");
+      ElapsedTime = 0;
+      
+      while (ElapsedTime < restDelay){
+  
+        CurrentTime = millis();
+        int hall_count_now=get_hall_count();
+        //updateHall(); // keep counting revolutions in state 2. Ghost continues to fly even when motor is off.
+            
+      
+        Serial.print(state);
+        Serial.print(", waiting for delay,  ");
+        Serial.print(ElapsedTime);
+        Serial.print(",  ");
         Serial.print(restDelay);
-    Serial.print(",  ");
-    Serial.println(hall_count);
-    
-      ElapsedTime = CurrentTime - startTime;
+        Serial.print(",  ");
+        Serial.println(hall_count_now);
+        ElapsedTime = CurrentTime - startTime;
+        delay(500);
     }
-    
-      numRot=hall_count + extraFlybackRevolution; // one more revolution on flyback to compensate for slip.
-      hall_count=0;
+
+      int hall_count_now=get_hall_count();
+      numRot=hall_count_now + extraFlybackRevolution; // one more revolution on flyback to compensate for slip.
+      reset_hall_count();
       state=3;
     }
 
